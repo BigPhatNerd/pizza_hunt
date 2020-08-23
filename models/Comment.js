662913b74090a9645b54552160bece2eea6114b2
Wilson <wilsonhorrell@git.training>
2020-08-23 15:52:14 -0500

    Finished with web app

---
+++ models/Comment.js
@@ -0,0 +1,54 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReplySchema = new Schema({
+    replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+        type: String
+    },
+    writtenBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYY [at] hh:mm a')
+    }
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+})
+
+const CommentSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    commentBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    replies: [ReplySchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+})
+
+const Comment = model("Comment", CommentSchema);
+
+module.exports = Comment;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,13 +10,29 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+
+    }]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 const Pizza = model("Pizza", PizzaSchema);
